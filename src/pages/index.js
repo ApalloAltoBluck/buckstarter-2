dccee67b29d60cca1d6be7214182579ac3a48652
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2021-11-08 18:05:28 -0500

    dada

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,55 @@
-import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import React, {useCallback} from "react"
+
+import ReactFullpage from '@fullpage/react-fullpage';
+
+
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+
+
+function IndexPage() {
+
+  return (
   <Layout>
     <Seo title="Home" />
     <section className="py-5 text-center container">
+        <ReactFullpage
+    //fullpage options
+    licenseKey = {'YOUR_KEY_HERE'}
+    scrollingSpeed = {1000} /* Options here */
+
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section bg-dark">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
           <h1 className="fw-light">Hello world ! </h1>
           <p className="lead text-muted"> Welcome to this Boostrap 5 Gatsby Starter</p>
+          <div id="fullpage">
+	<div class="section">Some section</div>
+	<div class="section">Some section</div>
+	<div class="section">Some section</div>
+	<div class="section">Some section</div>
+</div>
+
+     
           <StaticImage
             src="../images/gatsby-astronaut.png"
             width={300}
@@ -30,6 +67,7 @@ const IndexPage = () => (
       </div>
     </section>
   </Layout>
-)
+  )
+}
 
 export default IndexPage
