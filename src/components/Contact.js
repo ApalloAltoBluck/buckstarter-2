a8fe0b05d8e71fb725dedab5f5ea5c2a1c96d80f
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2021-11-12 20:34:28 -0500

    added Contact.js and .css for contact form
    also made some minor changes at the bottom of fullpage.css

---
+++ src/components/Contact.js
@@ -0,0 +1,60 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { useState } from 'react';
+import { send } from 'emailjs-com';
+
+import "./layout.scss"
+import "./Contact.css";
+
+
+function Contact() {
+    const [toSend, setToSend] = useState({
+      from_name: '',
+      to_name: 'Buckstarter',
+      message: '',
+      reply_to: '',
+    });
+  
+    const onSubmit = (e) => {
+      e.preventDefault();
+      send(
+        'service_cbpfuru',
+        'template_af1g3od',
+        toSend,
+        'user_CbLWqlZiNA0paGpLlwoNa'
+      )
+        .then((response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        })
+        .catch((err) => {
+          console.log('FAILED...', err);
+        });
+    };
+  
+    const handleChange = (e) => {
+      setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
+  
+    return (
+      <div className='contact'>
+        <form onSubmit={onSubmit}>
+          <p>Your Email</p>
+          <input type='text' id="email" name='reply_to' placeholder='name@example.com' value={toSend.reply_to} onChange={handleChange}/>
+            <br />
+          <p>your Company</p>
+          <input type='text' id="from" name='from_name' placeholder=' ' value={toSend.from_name} onChange={handleChange}/>
+            {/* <br />
+          <input type='text' id="to" name='to_name' placeholder='to name' value={toSend.to_name} onChange={handleChange}/> */}
+            <br />
+          <p>Message</p>
+          <input type='text' id="message" name='message' placeholder=' ' value={toSend.message} onChange={handleChange}/>
+            <br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    );
+  }
+
+export default Contact
