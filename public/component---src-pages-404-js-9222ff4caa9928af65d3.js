a797731a604f0cddc68f58cba16799ce9b9a6cec
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2021-12-21 14:34:44 -0500

    added content for public folder
    part 2

---
+++ public/component---src-pages-404-js-9222ff4caa9928af65d3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5=self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5||[]).push([[883],{9616:function(t,e,a){a.r(e);var r=a(7294),n=a(4832),s=a(3751);e.default=function(){return r.createElement(n.Z,null,r.createElement(s.Z,{title:"404: Not found"}),r.createElement("div",{className:"container text-center my-5"},r.createElement("h1",null,"404: Not Found"),r.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9222ff4caa9928af65d3.js.map
---
+++ public/component---src-pages-404-js-c0576281cac9dda97df8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5=self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5||[]).push([[883],{9616:function(t,e,a){a.r(e);var r=a(7294),n=a(4537),s=a(3751);e.default=function(){return r.createElement(n.Z,null,r.createElement(s.Z,{title:"404: Not found"}),r.createElement("div",{className:"container text-center my-5"},r.createElement("h1",null,"404: Not Found"),r.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c0576281cac9dda97df8.js.map
